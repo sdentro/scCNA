787ac4237886db5892b9721b935013db8b4acdc4
Stefan Dentro <sd11@sanger.ac.uk>
2018-09-02 08:31:30 +0100

    Adding alpha paramter to segmentation function export

--- scCNA/R/segmentTrack.R
+++ scCNA/R/segmentTrack.R
@@ -4,11 +4,12 @@ function(covtrack,
                          starts,
                          ends=NA,
                          sd=0,
-                         min.width=5)
+                         min.width=5,
+                         alpha = 0.01)
 {
     covtrack <- covtrack*rnorm(length(covtrack),mean=1,sd=sd)
     cna <- CNA(covtrack,chr=rep(chr,length(covtrack)),
                maploc=starts,data.type="logratio")
     cna.smoothed <- smooth.CNA(cna)
-    segment(cna.smoothed,min.width=min.width)
+    segment(cna.smoothed,min.width=min.width,alpha=alpha)
 }
